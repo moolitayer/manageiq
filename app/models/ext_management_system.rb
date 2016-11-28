033ee55aeb0ed2c1acf569d3721d10d23ac30ba6
Mooli Tayer <mtayer@redhat.com>
2016-11-28 14:31:06 +0200

    Force unique endpoint hostname only for same type

--- app/models/ext_management_system.rb
+++ app/models/ext_management_system.rb
@@ -71,9 +71,9 @@ class ExtManagementSystem < ApplicationRecord
   def hostname_uniqueness_valid?
     return unless hostname_required?
     return unless hostname.present? # Presence is checked elsewhere
+    # check uniqueness per provider type
 
-    existing_hostnames = Endpoint.where.not(:resource_id => id).pluck(:hostname).compact.map(&:downcase)
-
+    existing_hostnames = (self.class.all - [self]).map(&:hostname).compact.map(&:downcase)
     errors.add(:hostname, "has already been taken") if existing_hostnames.include?(hostname.downcase)
   end
 
