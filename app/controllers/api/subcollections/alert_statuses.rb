a06309c38eab2b3391c1e733756c51f4c1b15299
Mooli Tayer <mtayer@redhat.com>
2016-12-14 16:46:07 +0200

    [demo] hack to have different severities

--- app/controllers/api/subcollections/alert_statuses.rb
+++ app/controllers/api/subcollections/alert_statuses.rb
@@ -6,6 +6,14 @@ module Api
       def alert_statuses_query_resource(object)
         alerts = []
         alerts = object.miq_alert_statuses.collect(&:to_json) if object.respond_to?(:miq_alert_statuses)
+        if alerts.length == 2
+          alerts[0]['severity'] = 'warning'
+          alerts[1]['severity'] = 'warning'
+        end
+        if alerts.length > 2
+          alerts[1]['severity'] = 'warning'
+          alerts[2]['severity'] = 'danger'
+        end
         alerts
       end
     end
